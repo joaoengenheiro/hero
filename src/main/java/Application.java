5f53dd57c83e107b4d870b9a97049a6c433889ca
João <you@example.com>
2022-10-07 19:21:11 +0100

    Second version

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+public class Application {
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+ }
+}
--- src/main/java/com/joaoengenheiro/hero/Main.java
+++ src/main/java/com/joaoengenheiro/hero/Main.java
@@ -1,7 +1,35 @@
 package com.joaoengenheiro.hero;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+
+        try {
+
+            TerminalSize terminalSize = new TerminalSize(19, 12);
+            DefaultTerminalFactory terminalFactory = new
+                    DefaultTerminalFactory()
+                    .setInitialTerminalSize(terminalSize);
+
+            Terminal terminal = terminalFactory.createTerminal();
+
+            //Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if        necessary
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
